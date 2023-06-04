fa074133cfa1ce21e4966a7bb0518c207fc6c370
Alpisopian6 <2006130@itg.ac.id>
2023-06-04 20:54:28 +0700

    first commit

---
+++ lib/pages/add_note_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:simple_note/db/database_services.dart';
+import 'package:simple_note/models/note.dart';
+
+class AddNotePage extends StatefulWidget {
+  const AddNotePage({super.key, this.note});
+  final Note? note;
+
+  @override
+  State<AddNotePage> createState() => _AddNotePageState();
+}
+
+class _AddNotePageState extends State<AddNotePage> {
+  final DatabaseService dbService = DatabaseService();
+  late TextEditingController _titleController;
+  late TextEditingController _descController;
+
+  @override
+  void initState() {
+    _titleController = TextEditingController();
+    _descController = TextEditingController();
+    if (widget.note != null) {
+      _titleController.text = widget.note!.title;
+      _descController.text = widget.note!.desc;
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () async {
+          Note tempNote = Note(
+              title: _titleController.text,
+              desc: _descController.text,
+              createdAt: DateTime.now().toIso8601String());
+          if (widget.note != null) {
+            await dbService.updateNote(widget.note!.key, tempNote);
+            //if (context.mounted) return;
+            GoRouter.of(context).pop();
+          } else {
+            await dbService.addNote(tempNote);
+            //if (context.mounted) return;
+            GoRouter.of(context).pop();
+          }
+        },
+        label: Text("Simp"),
+        icon: const Icon(Icons.save),
+      ),
+      appBar: AppBar(
+        title: Text(
+          widget.note != null ? "Edit Note" : "Add note",
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _titleController,
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Judul Not",
+                    hintStyle:
+                        TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
+              ),
+              TextFormField(
+                maxLines: 150,
+                controller: _descController,
+                style: TextStyle(fontSize: 14),
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Deskripsina",
+                    hintStyle: TextStyle(fontSize: 14)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:simple_note/db/database_services.dart';
+import 'package:simple_note/extension/format_date.dart';
+import 'package:simple_note/models/note.dart';
+import 'package:simple_note/utils/app_routes.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final DatabaseService dbService = DatabaseService();
+
+  final GlobalKey<ScaffoldState> _sKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _sKey,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await GoRouter.of(context).pushNamed('add-note');
+          setState(() {});
+        },
+        child: Icon(Icons.note_add),
+      ),
+      appBar: AppBar(
+        title: const Text("Simple Notes"),
+        elevation: 0,
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box('notesBox').listenable(),
+        builder: (context, box, child) {
+          if (box.values.isEmpty) {
+            return Center(
+              child: Text("Tidak Ada Catatan"),
+            );
+          } else {
+            return ListView.separated(
+              separatorBuilder: (context, index) {
+                return SizedBox(
+                  height: 5,
+                );
+              },
+              itemCount: box.length,
+              itemBuilder: (context, index) {
+                Note currentNote = box.getAt(index);
+                return Dismissible(
+                  key: Key(currentNote.key.toString()),
+                  onDismissed: (_) async {
+                    dbService.deleteNote(currentNote).then((value) {
+                      ScaffoldMessenger.of(_sKey.currentContext!).showSnackBar(
+                          const SnackBar(content: Text("Catatan Dihapus")));
+                    });
+                  },
+                  child: NoteCard(note: currentNote),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class NoteCard extends StatelessWidget {
+  final Note note;
+  const NoteCard({super.key, required this.note});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ListTile(
+        onTap: () {
+          GoRouter.of(context).pushNamed(
+            AppRoutes.editNote,
+            extra: note,
+          );
+        },
+        title: Text(
+          note.title,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Text(
+          note.desc,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        trailing: Text(
+          'dibuat pada :\n${note.createdAt.formatDate()}',
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
